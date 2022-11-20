e8c2f2681b7156f7b1cbfbf174070a5db96e6ebd
KylaLiwanag <kylacamilleliwanag@gmail.com>
2022-11-20 23:23:53 +0800

    Used to get the repsonse

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
 import long_response as long
 
-def message_probability(user_message, recognised_words, singe_response=False, required_words=[]):
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
     message_certainty = 0
     has_required_words= True
 #Counts how many words are present in each predefined message
@@ -50,10 +50,10 @@ def check_all_mesagges(message):
 
 
 
-#
+#Used to get the response
 def get_response(user_input):
     split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
-    response = check_all_messages(split_message)
+    response = check_all_mesagges(split_message)
     return response
 
 # Testing the response system
