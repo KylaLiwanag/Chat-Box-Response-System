32c4c933cb89e78bd9df3272cd5a5edda4ea9b70
KylaLiwanag <kylacamilleliwanag@gmail.com>
2022-11-20 22:50:00 +0800

    Checks that the required words are in the string

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import long_response as long
 
 def message_probability(user_message, recognised_words, singe_response=False, required_words=[]):
     message_certainty = 0
-    has_required = True
+    has_required_words= True
 #Counts how many words are present in each predefined message
     for word in user_message:
         if word in recognised_words:
             message_certainty += 1
 #Calculates the percent of recognised words in a user message
     percentage = float(message_certainty) / float(len(recognised_words))
+
+#Checks that the required words are in the string
+    for word in required_words:
+        if word not in user_message:
+            has_required_words = False
+            break
 
 def get_response(user_input):
     split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
